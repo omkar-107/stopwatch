7e1c8b90c12be3f28f5e0c69668dd97f0fb412e5
omkar <salunkheomkar107@gmail.com>
2024-02-06 14:29:01 +0530

    Initial Commit

---
+++ stopwatch/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch
+//
+//  Created by mini project on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct stopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
